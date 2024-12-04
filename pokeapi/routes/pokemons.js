75fec4656679d4283bbbb77e747cd7150b9ec018
Hamid Sougouma Ali <92211673+Sougoumay@users.noreply.github.com>
2024-12-04 11:53:08 +0100

    Update pokemons.js

--- pokeapi/routes/pokemons.js
+++ pokeapi/routes/pokemons.js
@@ -5,6 +5,8 @@ async function routes(fastify, options) {
             const min_range = parseInt(request.query.start) || parseInt(process.env.POKEDEX_START_NUMBER) || 1;
             const max_range = parseInt(request.query.end) || parseInt(process.env.POKEDEX_END_NUMBER) || 1;
 
+            
+
             if (max_range < min_range) {
                 return fastify.httpErrors.badRequest("Parameter 'end' must be greater than 'start'");
             }
